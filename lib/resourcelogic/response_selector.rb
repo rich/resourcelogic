075d2d0667f6487bf0fb411bc20d94901b5c1fc5
Rich Cavanaugh <rich.cavanaugh@tycoonresearch.com>
2009-05-08 17:43:12 -0400

    adding the ResponseSelector which is able to selector appropriate options based on the context

---
+++ lib/resourcelogic/response_selector.rb
@@ -0,0 +1,36 @@
+module Resourcelogic
+  class ResponseSelector
+    attr_accessor :context
+    attr_accessor :default_options
+    attr_accessor :context_options
+    
+    def initialize(context, default_options, context_options)
+      self.context = context
+      self.default_options = default_options
+      self.context_options = context_options
+    end
+    
+    def response
+      (self.default_options.response + self.context_options.response).inject({}) do |h, v|
+        h[v.first] = v.last
+        h
+      end.to_a
+    end
+    
+    def before
+      @before ||= self.context_options.before || self.default_options.before
+    end
+    
+    def after
+      @after ||= self.context_options.after || self.default_options.after
+    end
+    
+    def flash
+      @flash ||= self.context_options.flash || self.default_options.flash
+    end
+    
+    def flash_now
+      @flash_now ||= self.context_options.flash_now || self.default_options.flash_now
+    end
+  end
+end
